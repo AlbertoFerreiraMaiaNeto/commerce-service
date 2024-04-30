a9c90a60d74541a06bf88c1f2571519ee5e7bce6
Alberto Neto <albertoferreiramaianeto@gmail.com>
2024-04-29 22:54:32 -0300

    fixing kafka configurations and add log messages/exceptions

--- src/main/java/org/acme/kafka/KafkaOrderProducer.java
+++ src/main/java/org/acme/kafka/KafkaOrderProducer.java
@@ -3,11 +3,13 @@ package org.acme.kafka;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import org.acme.persistence.dto.KafkaOrderDTO;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 @ApplicationScoped
+@Slf4j
 public class KafkaOrderProducer {
 
     @Inject
@@ -15,7 +17,12 @@ public class KafkaOrderProducer {
     Emitter<KafkaOrderDTO> emitter;
 
     public void publish(KafkaOrderDTO kafkaOrderDTO) {
-        this.emitter.send(kafkaOrderDTO);
+        try{
+            this.emitter.send(kafkaOrderDTO);
+            log.info("Sent message to Kafka, Order Owner: {}", kafkaOrderDTO.getOrderDTO().getOrderOwner());
+        } catch (Exception e) {
+            log.error("Error to trying send message to Kafka, Order Owner: {}", kafkaOrderDTO.getOrderDTO().getOrderOwner());
+        }
     }
 
 }
